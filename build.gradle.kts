b44facbc01112f4d4814e26107be4b33a8729165
stefankoppier <stefan.koppier@outlook.com>
2024-06-16 21:10:15 +0200

    Added build workflow and sonar analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,16 @@
+plugins {
+    alias(libs.plugins.sonarqube)
+}
+
 allprojects {
     group = "io.github.mappie"
     version = "0.1.0"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Mr-Mappie_mappe")
+        property("sonar.organization", "mappie")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
