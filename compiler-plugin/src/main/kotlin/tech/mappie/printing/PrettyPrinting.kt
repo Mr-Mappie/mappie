d1f52346f8e5c971e18849e84888a45f0b4fbc11
stefankoppier <stefan.koppier@outlook.com>
2024-06-23 12:21:46 +0200

    Small refactoring to test CI

--- compiler-plugin/src/main/kotlin/tech/mappie/printing/PrettyPrinting.kt
+++ compiler-plugin/src/main/kotlin/tech/mappie/printing/PrettyPrinting.kt
@@ -4,5 +4,5 @@ import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.types.getClass
 import org.jetbrains.kotlin.ir.types.isNullable
 
-fun IrType.pretty(): String =
+fun IrType.pretty() =
     getClass()!!.name.asString() + if (isNullable()) "?" else ""
