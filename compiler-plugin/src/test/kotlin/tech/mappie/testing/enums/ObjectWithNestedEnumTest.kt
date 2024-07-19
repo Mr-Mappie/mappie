512a3ba46532776ae755fca38d89912a5892e884
stefankoppier <stefan.koppier@outlook.com>
2024-07-19 09:42:30 +0200

    Added first test case

--- compiler-plugin/src/test/kotlin/tech/mappie/testing/enums/ObjectWithNestedEnumTest.kt
+++ compiler-plugin/src/test/kotlin/tech/mappie/testing/enums/ObjectWithNestedEnumTest.kt
@@ -1,6 +1,7 @@
 package tech.mappie.testing.enums
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import tech.mappie.testing.compilation.KotlinCompilation
@@ -17,6 +18,38 @@ class ObjectWithNestedEnumTest {
 
     @TempDir
     lateinit var directory: File
+
+    @Test
+    @Disabled
+    fun `map data classes with nested enum using object InnerMapper without declaring nested mapping should succeed`() {
+        KotlinCompilation(directory).apply {
+            sources = buildList {
+                add(
+                    kotlin("Test.kt",
+                        """
+                        import tech.mappie.api.ObjectMappie
+                        import tech.mappie.api.EnumMappie
+                        import tech.mappie.testing.enums.ObjectWithNestedEnumTest.*
+    
+                        class Mapper : ObjectMappie<Input, Output>()
+                        """
+                    )
+                )
+            }
+        }.compile {
+            assertThat(exitCode).isEqualTo(ExitCode.OK)
+            assertThat(messages).isEmpty()
+
+            val mapper = classLoader
+                .loadObjectMappieClass<Input, Output>("Mapper")
+                .constructors
+                .first()
+                .call()
+
+            assertThat(mapper.map(Input(InnerEnum.A)))
+                .isEqualTo(Output(OuterEnum.A))
+        }
+    }
 
     @Test
     fun `map data classes with nested enum using object InnerMapper without declaring mapping should succeed`() {
