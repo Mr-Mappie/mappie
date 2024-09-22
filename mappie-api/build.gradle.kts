429aaa370844f176eeff98f4faefe77ca7f34709
Stefan Koppier <stefan.koppier@outlook.com>
2024-09-22 08:46:51 +0200

    #88 Added multiplatform targets (#89)

--- mappie-api/build.gradle.kts
+++ mappie-api/build.gradle.kts
@@ -1,23 +1,60 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.android.library) apply false
     id("maven-publish")
 }
 
 kotlin {
     explicitApi()
+    withSourcesJar(publish = false)
+    applyDefaultHierarchyTemplate()
 
-    withSourcesJar()
+    jvm {
+        withSourcesJar(publish = true)
+    }
 
-    jvm()
+    androidNativeX64()
+    androidNativeArm64()
+
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
+
+    tvosX64()
+    tvosSimulatorArm64()
+    tvosArm64()
+
+    js {
+        browser()
+        nodejs()
+    }
+
+    wasmJs {
+        browser()
+        nodejs()
+    }
+
+    mingwX64()
+
+    macosX64()
+    macosArm64()
+
+    linuxX64()
+    linuxArm64()
 }
 
+val dokkaHtml by tasks.dokkaHtml
 tasks.register<Jar>("javadocJar") {
     group = "build"
     description = "Assemble a javadoc jar containing the Dokka pages of the 'main' feature."
     archiveClassifier = "javadoc"
-    from(layout.buildDirectory.dir("dokka/javadoc"))
-    dependsOn("dokkaJavadoc")
+    from(dokkaHtml.outputDirectory)
+    dependsOn(dokkaHtml)
 }
 
 publishing {
