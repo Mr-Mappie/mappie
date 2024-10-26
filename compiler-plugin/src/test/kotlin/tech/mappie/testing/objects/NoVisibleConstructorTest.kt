3f80428dbe66a41853cc15dd4eb6f4f45c75d9be
stefankoppier <stefan.koppier@outlook.com>
2024-10-26 17:58:14 +0200

    Revert introduced bug

--- compiler-plugin/src/test/kotlin/tech/mappie/testing/objects/NoVisibleConstructorTest.kt
+++ compiler-plugin/src/test/kotlin/tech/mappie/testing/objects/NoVisibleConstructorTest.kt
@@ -8,6 +8,7 @@ import java.io.File
 class NoVisibleConstructorTest {
 
     data class Input(val name: String)
+    @ConsistentCopyVisibility
     data class Output private constructor(val name: String)
 
     @TempDir
