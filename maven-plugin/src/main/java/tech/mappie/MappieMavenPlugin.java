2526b1ac62bd42a296b455e153f88c858b434336
stefankoppier <stefan.koppier@outlook.com>
2025-02-13 18:05:36 +0100

    Uncommented MappieMavenPlugin

--- maven-plugin/src/main/java/tech/mappie/MappieMavenPlugin.java
+++ maven-plugin/src/main/java/tech/mappie/MappieMavenPlugin.java
@@ -11,28 +11,28 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
-//@Component(role = KotlinMavenPluginExtension.class, hint = "mappie-compiler-plugin")
-//public class MappieMavenPlugin implements KotlinMavenPluginExtension {
-//    @Override
-//    public boolean isApplicable(@NotNull MavenProject project, @NotNull MojoExecution execution) {
-//        return true;
-//    }
-//
-//    @Override
-//    public String getCompilerPluginId() {
-//        try (var resource = getClass().getClassLoader().getResourceAsStream("version.properties")) {
-//            var properties = new Properties();
-//            properties.load(resource);
-//            var version = properties.getProperty("version");
-//            return "tech.mappie:mappie-compiler-plugin:" + version;
-//        } catch (IOException e) {
-//            throw new IllegalStateException("Failed to load version of Mappie.");
-//        }
-//    }
-//
-//    @NotNull
-//    @Override
-//    public List<PluginOption> getPluginOptions(@NotNull MavenProject project, @NotNull MojoExecution execution) {
-//        return List.of();
-//    }
-//}
+@Component(role = KotlinMavenPluginExtension.class, hint = "mappie-compiler-plugin")
+public class MappieMavenPlugin implements KotlinMavenPluginExtension {
+    @Override
+    public boolean isApplicable(@NotNull MavenProject project, @NotNull MojoExecution execution) {
+        return true;
+    }
+
+    @Override
+    public String getCompilerPluginId() {
+        try (var resource = getClass().getClassLoader().getResourceAsStream("version.properties")) {
+            var properties = new Properties();
+            properties.load(resource);
+            var version = properties.getProperty("version");
+            return "tech.mappie:mappie-compiler-plugin:" + version;
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to load version of Mappie.");
+        }
+    }
+
+    @NotNull
+    @Override
+    public List<PluginOption> getPluginOptions(@NotNull MavenProject project, @NotNull MojoExecution execution) {
+        return List.of();
+    }
+}
