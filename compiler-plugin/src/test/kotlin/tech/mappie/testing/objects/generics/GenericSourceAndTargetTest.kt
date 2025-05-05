20bf7c2d86355e95e4bb7b48f02b39aa6d668e58
stefankoppier <stefan.koppier@outlook.com>
2025-05-05 10:58:27 +0200

    #197 Fixed wrong rename of tests

--- compiler-plugin/src/test/kotlin/tech/mappie/testing/objects/generics/GenericSourceAndTargetTest.kt
+++ compiler-plugin/src/test/kotlin/tech/mappie/testing/objects/generics/GenericSourceAndTargetTest.kt
@@ -7,7 +7,7 @@ import tech.mappie.testing.compilation.compile
 import tech.mappie.testing.loadObjectMappieClass
 import java.io.File
 
-class GenericSourceAndInputTest {
+class GenericSourceAndTargetTest {
 
     data class Input<A, B>(
         val a: A,
