1119ce7f346ca1028000cab162cdfdba94ef506b
ErwinOlie <github@erwinolie.nl>
2025-09-19 11:00:59 +0200

    show all problems when having multiple complex inner objects

---
+++ compiler-plugin/src/test/kotlin/tech/mappie/testing/objects/GeneratedClassFailMultipleTest.kt
@@ -0,0 +1,48 @@
+package tech.mappie.testing.objects
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import tech.mappie.testing.compilation.compile
+import java.io.File
+
+class GeneratedClassFailMultipleTest {
+    data class Input(val a: InnerInputA, val b: InnerInputB)
+    data class InnerInputA(val value: String)
+    data class InnerInputB(val value: String)
+    data class Output(val a: InnerOutputA, val b: InnerOutputB)
+    data class InnerOutputA(val value: Int)
+    data class InnerOutputB(val value: Int)
+
+    @TempDir
+    lateinit var directory: File
+
+    @Test
+    fun `map object with multiple nested classes without declaring mapping should fail all`() {
+        compile(directory) {
+            file("Test.kt",
+                """
+                import tech.mappie.api.ObjectMappie
+                import tech.mappie.testing.objects.GeneratedClassFailMultipleTest.*
+
+                class Mapper : ObjectMappie<Input, Output>()
+                """
+            )
+        } satisfies {
+            isCompilationError()
+            hasErrorMessage(
+                4,
+                "No implicit mapping can be generated from InnerInputA to InnerOutputA",
+                listOf(
+                    "Target InnerOutputA::value automatically resolved from InnerInputA::value but cannot assign source type String to target type Int"
+                )
+            )
+            hasErrorMessage(
+                4,
+                "No implicit mapping can be generated from InnerInputB to InnerOutputB",
+                listOf(
+                    "Target InnerOutputB::value automatically resolved from InnerInputB::value but cannot assign source type String to target type Int"
+                )
+            )
+        }
+    }
+}
