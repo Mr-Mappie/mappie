a32471841b53178dee2283662e743965f4054945
Stefan Koppier <stefan.koppier@outlook.com>
2025-10-03 19:22:15 +0200

    Fix mapper detection bug (#282)
    * Fixes
    
    * Disabled tests

--- compiler-plugin/src/test/kotlin/tech/mappie/testing/objects/NestedNullToNonNullPropertyTest.kt
+++ compiler-plugin/src/test/kotlin/tech/mappie/testing/objects/NestedNullToNonNullPropertyTest.kt
@@ -1,6 +1,8 @@
 package tech.mappie.testing.objects
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import tech.mappie.testing.MappieTestCase
 
@@ -135,6 +137,74 @@ class NestedNullToNonNullPropertyTest : MappieTestCase() {
 
             assertThat(mapper.map(Input(InnerInput("test"), 1)))
                 .isEqualTo(Output(InnerOutput("test"), 1))
+        }
+    }
+
+    @Test
+    @Disabled("Explicit nullable mapping source in Mappie is not supported yet.")
+    fun `map object with nested null to non-null with implicit null mapper should succeed`() {
+        compile {
+            file("Test.kt",
+                """
+                import tech.mappie.api.ObjectMappie
+                import tech.mappie.testing.objects.NestedNullToNonNullPropertyTest.*
+
+                class Mapper : ObjectMappie<Input, Output>() { 
+                    override fun map(from: Input) = mapping {
+                        to::text fromProperty from::text
+                    }
+                }
+
+                object InnerMapper : ObjectMappie<InnerInput?, InnerOutput>() {
+                    override fun map(from: InnerInput?) = if (from == null) error("error") else mapping()
+                }
+                """
+            )
+        } satisfies {
+            isOk()
+            hasNoWarningsOrErrors()
+
+            val mapper = objectMappie<Input, Output>()
+
+            assertThat(mapper.map(Input(InnerInput("test"), 1)))
+                .isEqualTo(Output(InnerOutput("test"), 1))
+
+            assertThatThrownBy { mapper.map(Input(null, 1)) }
+                .isInstanceOf(IllegalStateException::class.java)
+        }
+    }
+
+    @Test
+    @Disabled("Explicit nullable mapping source in Mappie is not supported yet.")
+    fun `map object with nested null to non-null with explicit null mapper should succeed`() {
+        compile {
+            file("Test.kt",
+                """
+                import tech.mappie.api.ObjectMappie
+                import tech.mappie.testing.objects.NestedNullToNonNullPropertyTest.*
+
+                class Mapper : ObjectMappie<Input, Output>() { 
+                    override fun map(from: Input) = mapping {
+                        to::text fromProperty from::text via InnerMapper
+                    }
+                }
+
+                object InnerMapper : ObjectMappie<InnerInput?, InnerOutput>() {
+                    override fun map(from: InnerInput?) = if (from == null) error("error") else mapping()
+                }
+                """
+            )
+        } satisfies {
+            isOk()
+            hasNoWarningsOrErrors()
+
+            val mapper = objectMappie<Input, Output>()
+
+            assertThat(mapper.map(Input(InnerInput("test"), 1)))
+                .isEqualTo(Output(InnerOutput("test"), 1))
+
+            assertThatThrownBy { mapper.map(Input(null, 1)) }
+                .isInstanceOf(IllegalStateException::class.java)
         }
     }
 }
