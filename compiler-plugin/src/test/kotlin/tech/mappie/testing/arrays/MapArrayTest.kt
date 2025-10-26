4074da182c48523dd51b48d43b70da5334120556
ErwinOlie <github@erwinolie.nl>
2025-10-26 11:34:49 +0100

    updated MapArrayTest to use MappieTestCase as base

--- compiler-plugin/src/test/kotlin/tech/mappie/testing/arrays/MapArrayTest.kt
+++ compiler-plugin/src/test/kotlin/tech/mappie/testing/arrays/MapArrayTest.kt
@@ -2,26 +2,20 @@ package tech.mappie.testing.arrays
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.io.TempDir
 import tech.mappie.api.mapArray
-import tech.mappie.testing.compilation.compile
-import tech.mappie.testing.loadObjectMappieClass
-import java.io.File
+import tech.mappie.testing.MappieTestCase
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-class MapArrayTest {
+class MapArrayTest : MappieTestCase() {
 
     data class Input(val value: LocalDateTime)
 
     data class Output(val value: LocalDate)
 
-    @TempDir
-    lateinit var directory: File
-
     @Test
     fun `mapArray implicit succeed`() {
-        compile(directory) {
+        compile {
             file("Test.kt",
                 """
                 import tech.mappie.api.ObjectMappie
@@ -34,11 +28,7 @@ class MapArrayTest {
             isOk()
             hasNoWarningsOrErrors()
 
-            val mapper = classLoader
-                .loadObjectMappieClass<Input, Output>("Mapper")
-                .constructors
-                .first()
-                .call()
+            val mapper = objectMappie<Input, Output>()
 
             assertThat(mapper.mapArray(arrayOf(Input(LocalDateTime.MIN), Input(LocalDateTime.MAX))))
                 .isEqualTo(arrayOf(Output(LocalDate.MIN), Output(LocalDate.MAX)))
@@ -47,7 +37,7 @@ class MapArrayTest {
 
     @Test
     fun `mapArray implicit and empty succeed`() {
-        compile(directory) {
+        compile {
             file("Test.kt",
                 """
                 import tech.mappie.api.ObjectMappie
@@ -60,11 +50,7 @@ class MapArrayTest {
             isOk()
             hasNoWarningsOrErrors()
 
-            val mapper = classLoader
-                .loadObjectMappieClass<Input, Output>("Mapper")
-                .constructors
-                .first()
-                .call()
+            val mapper = objectMappie<Input, Output>()
 
             assertThat(mapper.mapArray(emptyArray()))
                 .isInstanceOf(Array<Output>::class.java)
