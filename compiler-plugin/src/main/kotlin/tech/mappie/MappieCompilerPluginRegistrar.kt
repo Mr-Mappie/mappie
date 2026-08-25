777a4566c0a9fbd558e46ea354db84dac2ead8f9
Erwin Olie <github@erwinolie.nl>
2026-08-25 17:38:02 +0200

    fix issue #298 : Load external mappers via SPI

--- compiler-plugin/src/main/kotlin/tech/mappie/MappieCompilerPluginRegistrar.kt
+++ compiler-plugin/src/main/kotlin/tech/mappie/MappieCompilerPluginRegistrar.kt
@@ -11,20 +11,16 @@ import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CommonConfigurationKeys.MESSAGE_COLLECTOR_KEY
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrarAdapter
-import org.jetbrains.kotlin.konan.file.File
 import tech.mappie.MappieCommandLineProcessor.Companion.ARGUMENT_OUTPUT_DIR
 import tech.mappie.MappieCommandLineProcessor.Companion.ARGUMENT_REPORT_DIR
 import tech.mappie.MappieCommandLineProcessor.Companion.ARGUMENT_REPORT_ENABLED
 import tech.mappie.MappieCommandLineProcessor.Companion.ARGUMENT_STRICTNESS_JAVA_NULLABILITY
 import tech.mappie.MappieCommandLineProcessor.Companion.ARGUMENT_USE_DEFAULT_ARGUMENTS
 import tech.mappie.MappieCommandLineProcessor.Companion.ARGUMENT_WARNINGS_AS_ERRORS
-import tech.mappie.compiler_plugin.BuildConfig
 import tech.mappie.config.MappieConfiguration
-import tech.mappie.config.MappieModule
 import tech.mappie.config.options.NamingConventionMode
 import tech.mappie.fir.MappieFirRegistrar
 import tech.mappie.ir.MappieIrRegistrar
-import java.util.EnumSet
 
 @OptIn(ExperimentalCompilerApi::class)
 class MappieCompilerPluginRegistrar : CompilerPluginRegistrar() {
@@ -35,14 +31,10 @@ class MappieCompilerPluginRegistrar : CompilerPluginRegistrar() {
 
     override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
         val config = MappieConfiguration(
-            modules = EnumSet.noneOf(MappieModule::class.java).apply {
-                if (configuration.isStartedWithDependency(MODULE_KOTLINX_DATETIME_REGEX)) {
-                    add(MappieModule.KOTLINX_DATETIME)
-                }
-                if (configuration.isStartedWithDependency(MODULE_KOTLINX_COLLECTIONS_IMMUTABLE_REGEX)) {
-                    add(MappieModule.KOTLINX_COLLECTIONS_IMMUTABLE)
-                }
-            },
+            classpath = configuration.moduleChunk
+                ?.modules
+                ?.flatMap { it.getClasspathRoots() }
+                .orEmpty(),
             isMappieDebugMode = configuration.isStartedWithDependency(TESTUTIL_REGEX),
             warningsAsErrors = configuration[ARGUMENT_WARNINGS_AS_ERRORS, false],
             useDefaultArguments = configuration[ARGUMENT_USE_DEFAULT_ARGUMENTS, true],
@@ -66,18 +58,8 @@ class MappieCompilerPluginRegistrar : CompilerPluginRegistrar() {
             ?: false
 
     companion object {
-        private val SEPARATOR = Regex.escapeReplacement(File.separator)
-
-        private val MODULE_KOTLINX_DATETIME_REGEX = Regex(
-            "(.*modules${SEPARATOR}kotlinx-datetime${SEPARATOR}build${SEPARATOR}classes${SEPARATOR}kotlin${SEPARATOR}jvm${SEPARATOR}main)|(.*module-kotlinx-datetime.*-${BuildConfig.VERSION}.*)"
-        )
-
-        private val MODULE_KOTLINX_COLLECTIONS_IMMUTABLE_REGEX = Regex(
-            "(.*modules${SEPARATOR}kotlinx-collections-immutable${SEPARATOR}build${SEPARATOR}classes${SEPARATOR}kotlin${SEPARATOR}jvm${SEPARATOR}main)|(.*module-kotlinx-collections-immutable.*-${BuildConfig.VERSION}.*)"
-        )
-
         private val TESTUTIL_REGEX = Regex(
-            ".*testutil-${BuildConfig.VERSION}.*\\.jar"
+            ".*testutil-.*\\.jar"
         )
     }
 }
